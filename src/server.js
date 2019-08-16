cbbde096b472a5821d51367268a7a1fbda2b1ccb
murilokrugner <murilokrugner@hotmail.com>
2019-08-16 13:12:40 -0300

    fix: server socket.io

--- src/server.js
+++ src/server.js
@@ -1,3 +1,18 @@
-import app from './app';
+import express from 'express';
+import cors from 'cors';
 
-app.listen(3333);
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+app.use((req, res, next) => {
+  req.io = io;
+
+  next();
+});
+
+app.use(cors());
+
+app.use(require('./routes'));
+
+server.listen(3333);
